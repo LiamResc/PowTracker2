61cd11ef4a4ba1b9e86d61022cdc02c86ca715b0
Yaaqoob Choulli <yaaqoobchoulli@Yaaqoobs-MacBook-Pro.local>
2024-01-06 14:22:32 -0700

    Done the marmot location in the correct format for the dictionary

--- scraper.py
+++ scraper.py
@@ -2,7 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-def skimarmot_scraper():
+resort_dict = {}
+
+def skimarmot_scraper(resort_dict):
 
     url = "https://www.skimarmot.com/mountain/weather-conditions/"
 
@@ -17,28 +19,29 @@ def skimarmot_scraper():
         # Find the element containing the weather information
         weather_group_elements = soup.find_all('div', class_='weather-group')
 
-        # Initialize dictionaries to store numerical values
-        snow_dict = {}
-        temperature_dict = {}
-
         for index, weather_group_element in enumerate(weather_group_elements):
             # Check for snow or temperature based on index or other criteria
             if index == 0:
                 snow_info = weather_group_element.text.strip()
                 snow_numbers = [int(num) for num in re.findall(r'\b\d+\b', snow_info)]
+                snow_numbers[7] = "N/A"
                 selected_values = [snow_numbers[i] for i in [5, 7, 8, 9]]
-                snow_labels = ['Last 24 Hours', '2 Days', 'Snow Base', 'Season Total']
-                snow_dict = dict(zip(snow_labels, selected_values))
             elif index == 1:
                 temperature_info = weather_group_element.text.strip()
                 temperature_numbers = [int(num) for num in re.findall(r'-?\b\d+\b', temperature_info)]            
-                temperature_labels = ['Mid Mountain 1980m', 'Upper Mountain 2612m']
-                temperature_dict = dict(zip(temperature_labels, temperature_numbers[1:]))
 
-        # Print or use the dictionaries as needed
-        print(f"Snow Dictionary (cm): {snow_dict}")
-        print(f"Temperature Dictionary (°C): {temperature_dict}")
+        
+        #values = [latitude, longitude, 24 hour snowfall, 7 day snowfall , Snow base, Seasonal snowfall, Current temperature]
+        values = [52.8005917, -118.0833546] + selected_values + [temperature_numbers[1]]
+
+        resort_dict['marmot'] = values
+
+        return resort_dict
 
     else:
         print(f"Failed to retrieve the page. Status Code: {response.status_code}")
-skimarmot_scraper()
+
+
+
+
+print(skimarmot_scraper(resort_dict))
