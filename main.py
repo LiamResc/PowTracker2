de2d2d9276db5cadc7d793b70a0a9257544785d0
Marcus Eshleman <marcuseshleman@gmail.com>
2024-01-06 18:08:29 -0700

    second temp change

--- main.py
+++ main.py
@@ -7,19 +7,30 @@ from branca.colormap import LinearColormap
 data = pd.DataFrame({
     'Resort': ['Marmott', 'Sunshine', 'Lake Lousie', 'Silver Star', 'Whistler', 'Big White', 'Sun Peaks', 'Revelstoke'],
     'Snowfall': [15, 22, 10, 30, 18, 5, 50, 15],
-    'Temperature': [-10, 5, 8, -3, 2, 2, 2, 2],
+    'Temperature': [-10, 5, 8, -3, 2, 2, 2, 20],
     'Latitude': [52.8005917, 51.0996056, 51.4249668, 50.35988235473633, 50.1171903, 49.715991, 50.8787669, 50.958621978759766],
     'Longitude': [-118.0833546, -115.772541, -116.177535, -119.0580596923828, -122.9543022, -118.933872, -119.8930944, -118.16400909423828]
 })
 
 # Create a map centered around Canada
-snowfall_map = folium.Map(location=[56.1304, -106.3468], zoom_start=4)
+"""
+province_input = inputs['province']
+if province_input = Alberta:
+
+
+
+"""
+starting_location = [56.1304, -106.3468]
+
+
+
+snowfall_map = folium.Map(location= starting_location, zoom_start=4)
 
 # Add the MousePosition plugin for hover information
 plugins.MousePosition().add_to(snowfall_map)
 
 
- max_temp = max(data['Temperature'], key=abs)
+max_temp = max(data['Temperature'], key=abs)
 # Use colormap for temperature (LinearColormap)
 temperature_colormap = LinearColormap(['blue', 'white', 'red'], vmin = -(max_temp), vmax=(max_temp))
 
