47c63095e4e19aa9babb7cb45658e81a137b269f
Marcus Eshleman <marcuseshleman@gmail.com>
2024-01-07 10:18:07 -0700

    done

--- main.updated/combined_msc_file.py
+++ main.updated/combined_msc_file.py
@@ -287,7 +287,65 @@ def main(inputs):
             
         else:
             print(f"Failed to retrieve the page. Status Code: {response.status_code}")
-    
+
+    def weatherGranby():
+        url = "https://weather.gc.ca/city/pages/qc-5_metric_e.html"
+
+        # Send a GET request to the URL
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            # Parse the HTML content using BeautifulSoup
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            # Find the specific element containing the current temperature
+            temperature_element = soup.find("p", class_="mrgn-bttm-sm lead no-obs-icon")
+
+            # Extract and print the current temperature
+            temperature = temperature_element.text.strip() if temperature_element else "N/A"
+            temperature_numeric = re.search(r'-?\d+(.\d+)?', temperature)
+            return int(temperature_numeric.group())
+        else:
+            temp = 0
+            return temp
+    def bromont_scraper(resort_dict):
+        # Replace 'YOUR_URL_HERE' with the actual URL of the website
+        url = 'https://www.bromontmontagne.com/en/detailed-conditions/'
+
+        # Make a request to the website and get the HTML content
+        response = requests.get(url)
+        html_content = response.content
+
+        # Parse the HTML content with BeautifulSoup
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        # Find all elements with the class 'snowreport-snowfall-box-value-element'
+        snowfall_element = soup.find(class_='data_metric infos-contitions txt-data-big')
+        snowfall_element2 = soup.find_all(class_='data_metric infos-contitions txt-data')
+
+
+        # Extract the text from each element and collect the numbers
+        snowfall_values1 = [element.get_text(strip=True) for element in snowfall_element2]
+        snowfall_value2= snowfall_element.text.strip()#[element.get_text(strip=True) for element in snowfall_element]
+
+        values= list(snowfall_value2.strip(' cm')) + snowfall_values1
+
+        values.remove(values[1])
+
+        values.append(weatherGranby())
+        values[0]=snowfall_value2.strip('cm')
+        values.insert(3,values[3])
+        values.remove(values[1])
+
+        latitude='45.305'
+        longitude='-72.637'
+        values.insert(0,latitude)
+        values.insert(1,longitude)
+
+        resort_dict['Bromont Mountain, QC'] = values
+        return resort_dict
+
+    bromont_scraper(resort_dict)
     lemassif(resort_dict)
     pano_scraper(resort_dict)
     bigwhite_scraper(resort_dict)
